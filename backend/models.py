19059cf5b056b94e3ab568b8780c57499a30f21e
Esosa Ohangbon <esosaohangbon68@gmail.com>
2024-12-16 19:07:17 -0500

    Added function for adding migrations

--- backend/models.py
+++ backend/models.py
@@ -9,3 +9,12 @@ class Migrations(db.model):
 
     def __repr__(self):
         return f"<Migration {self.spotify_user_id} - {self.track_id}>"
+    
+def add_migration(spotify_user_id, track_id):
+    migration = Migrations(
+        spotify_user_id=spotify_user_id,
+        track_id=track_id,
+        migrated_at=datetime.utcnow()
+    )
+    db.session.add(migration)
+    db.session.commit()
