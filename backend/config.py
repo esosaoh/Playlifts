263c8ec0a0f6b9f5a132010c2e8c6833d7d9c710
Esosa Ohangbon <esosaohangbon68@gmail.com>
2024-12-30 17:11:52 -0500

    Changed CORS config to allow credentials and restrict origins

--- backend/config.py
+++ backend/config.py
@@ -5,12 +5,20 @@ import os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-CORS(app)
-load_dotenv()
+CORS(app, supports_credentials=True, resources={
+    r"/*": {
+        "origins": ["http://localhost:5173"], 
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type"],
+        "supports_credentials": True
+    }
+})
+load_dotenv(override=True)
 
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:{MYSQL_PASSWORD}@localhost/migrations'
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{MYSQL_PASSWORD}@localhost/migrations'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/song_migrations'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
