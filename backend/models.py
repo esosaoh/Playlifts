432be104898f72d1be0f5047e597d57bf4fc805f
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-01-04 16:16:42 -0500

    Added create_user method

--- backend/models.py
+++ backend/models.py
@@ -20,3 +20,25 @@ class User(db.Model):
             "created_at": self.created_at,
             "last_login": self.last_login
         }
+    
+    @staticmethod
+    def create_user(spotify_id, email, access_token):
+        user = User.query.filter_by(spotify_id=spotify_id).first()
+        if user:
+            print(f"✅ User {email} already exists.")
+            # Optionally update the last login time or token
+            user.last_login = datetime.utcnow()
+            if access_token:
+                user.spotify_token = access_token
+            db.session.commit()
+        else:
+            user = User(
+                spotify_id=spotify_id,
+                email=email,
+                last_login=datetime.utcnow(),
+                spotify_token=access_token
+            )
+            db.session.add(user)
+            db.session.commit()
+            print(f"✅ New user {email} created successfully.")
+        return user
