d0b982d3fce04f8b73ee5552db4fc629c89293a5
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-07-17 04:15:19 -0400

    updated `APP_DIR`
    Signed-off-by: Esosa Ohangbon <esosaohangbon68@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -e 
+set -e
 
-APP_DIR="/home/ec2-user/backend"
+APP_DIR="/home/ec2-user"
 
-echo ">>> Deploying latest backend code..."
+echo ">>> Deploying latest code..."
 cd $APP_DIR
 
 echo ">>> Pulling latest code from GitHub..."
@@ -15,7 +15,7 @@ echo ">>> Activating virtual environment..."
 source venv/bin/activate
 
 echo ">>> Installing dependencies..."
-pip install -r requirements.txt
+pip install -r backend/requirements.txt
 
 echo ">>> Restarting backend service..."
 sudo systemctl restart api
