2a2e0b79d6ed70f10487a2ea2f6d97f2e4f9cabc
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-07-17 03:34:39 -0400

    fixed config bug

--- backend/config.py
+++ backend/config.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 CORS(app, supports_credentials=True, resources={
     r"/*": {
-        "origins": ["http://localhost:5173"], 
+        "origins": ["https://playlifts.com", "https://www.playlifts.com"], 
         "methods": ["GET", "POST", "OPTIONS"],
         "allow_headers": ["Content-Type"],
         "supports_credentials": True
