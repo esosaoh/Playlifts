8f785b52ad325ec931bdce00e9770c43696199d6
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-07-18 04:03:06 -0400

    updated for https

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,20 +4,28 @@ set -e
 source ./backend/.env  
 
 echo ">>> Copying files to EC2..."
-scp -i "$KEY_PATH" -r backend "$EC2_USER@$EC2_HOST:$REMOTE_DIR"
+scp -i "$KEY_PATH" -r backend deployment Dockerfile.* docker-compose.yml scripts "$EC2_USER@$EC2_HOST:$REMOTE_DIR/playlifts"
 
 echo ">>> Restarting backend Docker container..."
 
 ssh -i "$KEY_PATH" "$EC2_USER@$EC2_HOST" << EOF
+cd $REMOTE_DIR/playlifts
+
 docker stop ec2-user-backend-1 || true
 docker rm ec2-user-backend-1 || true
-docker build -t playlifts-api -f $REMOTE_DIR/Dockerfile.api $REMOTE_DIR
-docker run -d --name ec2-user-backend-1 -p 8889:8889 -e REDIS_URL=$REDIS_URL playlifts-api
+docker build -t playlifts-api -f Dockerfile.api .
+docker run -d --name ec2-user-backend-1 -p 8889:8889 \
+  -e REDIS_URL=$REDIS_URL \
+  -e OAUTHLIB_INSECURE_TRANSPORT=1 \
+  playlifts-api
 
 docker stop ec2-user-celery-worker-1 || true
 docker rm ec2-user-celery-worker-1 || true
-docker build -t playlifts-celery -f $REMOTE_DIR/Dockerfile.celery $REMOTE_DIR
-docker run -d --name ec2-user-celery-worker-1 -e REDIS_URL=$REDIS_URL playlifts-celery
+docker build -t playlifts-celery -f Dockerfile.celery .
+docker run -d --name ec2-user-celery-worker-1 \
+  -e REDIS_URL=$REDIS_URL \
+  -e OAUTHLIB_INSECURE_TRANSPORT=1 \
+  playlifts-celery
 EOF
 
 echo ">>> Deployment complete!"
