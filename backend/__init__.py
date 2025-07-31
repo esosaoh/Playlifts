b8555bd36faf5e90fac61fbf0986ca7b40c390fb
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-07-31 05:28:44 -0400

    update `__init__.py`
    Signed-off-by: Esosa Ohangbon <esosaohangbon68@gmail.com>

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,1 +1,26 @@
-# This file makes the backend directory a Python package
+"""
+Playlifts Backend Package
+
+A Flask-based backend service for transferring playlists between Spotify and YouTube Music.
+
+Author: esosaoh
+Version: 1.0.0
+Description: Backend API for the Playlifts playlist transfer application
+"""
+
+__version__ = "1.0.0"
+__author__ = "esosaoh"
+__description__ = "Backend API for the Playlifts playlist transfer application"
+__url__ = "https://github.com/esosaoh/Playlifts"
+
+# Package imports
+from .app import app
+from .models import *
+from .spotify_client import SpotifyClient
+from .youtube_client import YouTubeClient
+
+__all__ = [
+    'app',
+    'SpotifyClient', 
+    'YouTubeClient',
+]
