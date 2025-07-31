ba9be110aa516f3a88bc6f6476ad1f2287e5bfac
Esosa Ohangbon <esosaohangbon68@gmail.com>
2025-07-31 07:21:41 -0400

    remove mysql from config
    Signed-off-by: Esosa Ohangbon <esosaohangbon68@gmail.com>

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
@@ -17,10 +16,6 @@ CORS(app, supports_credentials=True, resources={
     }
 })
 
-MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{MYSQL_PASSWORD}@localhost/migrations'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-db = SQLAlchemy(app)
 
